e8b2feefc985503230a870c646b8c0a925d46c00
Byung-chun Yoo <yoobc1@gmail.com>
2017-12-16 21:21:07 -0500

    adding Publisher entity, publisherRepository, and bootstrap it

--- src/main/java/yoo/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/yoo/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -5,40 +5,59 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import yoo.springframework.spring5webapp.model.Author;
 import yoo.springframework.spring5webapp.model.Book;
+import yoo.springframework.spring5webapp.model.Publisher;
 import yoo.springframework.spring5webapp.repositories.AuthorRepository;
 import yoo.springframework.spring5webapp.repositories.BookRepository;
+import yoo.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     //DI by constructor
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData(){
 
+//        Publisher publisher = new Publisher();
+//        publisher.setName("newPub");
+//
+//        publisherRepository.save(publisher);
+
+
+
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher hc = new Publisher("Harper Collins", "New York");
+
+        Book ddd = new Book("Domain Driven Design", "1234", hc);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         //post data to db
+        //publisherRepository must be before bookRepository
+        //as book db needs to have publisher id
         authorRepository.save(eric);
+        publisherRepository.save(hc);
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Work");
+        Publisher work = new Publisher("Work", "New Mexico");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", work);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
         authorRepository.save(rod);
+        publisherRepository.save(work);
         bookRepository.save(noEJB);
-
     }
 
     @Override
--- src/main/java/yoo/springframework/spring5webapp/model/Book.java
+++ src/main/java/yoo/springframework/spring5webapp/model/Book.java
@@ -12,22 +12,25 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
     inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+
     public Book(){}
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -58,20 +61,20 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
     public Set<Author> getAuthors() {
         return authors;
     }
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
